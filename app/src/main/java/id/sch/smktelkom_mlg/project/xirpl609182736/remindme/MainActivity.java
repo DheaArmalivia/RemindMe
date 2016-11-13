06993c4c22546176480fc63e811c1ab3e8e7bf22
Dhea Armalivia <daffyderr@gmail.com>
2016-11-13 10:46:33 +0700

    Button Start

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl609182736/remindme/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl609182736/remindme/MainActivity.java
@@ -1,13 +1,28 @@
 package id.sch.smktelkom_mlg.project.xirpl609182736.remindme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bNext = (Button) findViewById(R.id.startButton);
+
+        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, remind.class));
+            }
+        });
+
     }
 }
